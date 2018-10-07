3fef0055e799787d068ca59faeff1220d11f153f
Teng Yan <tyan@CNtyan.local>
2018-10-07 17:46:14 +0800

    change zuul config

--- todo-service/src/main/java/com/example/training/todoservice/feign/UserFeignClient.java
+++ todo-service/src/main/java/com/example/training/todoservice/feign/UserFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "user-service",url = "http://user-service:8082")
+@FeignClient(name = "user-service",url = "http://localhost:8083")
 public interface UserFeignClient {
     @GetMapping("/users/verification")
     public User verificationUser(@RequestHeader("token") String token);
